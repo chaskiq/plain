3d704e7bdfde77f8b662420223212e49c87ee17c
Miguel Michelson <miguelmichelson@gmail.com>
2023-08-20 17:53:50 -0400

    fix migrations

--- db/migrate/20230729010703_create_plain_conversations.rb
+++ db/migrate/20230729010703_create_plain_conversations.rb
@@ -2,7 +2,8 @@ class CreatePlainConversations < ActiveRecord::Migration[7.0]
   def change
     create_table :plain_conversations do |t|
       t.string :subject
-
+      t.datetime :pinned_at
+      t.boolean :pinned
       t.timestamps
     end
   end
